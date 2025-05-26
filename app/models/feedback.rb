6781559682ddcc19aed48bf93f1230f401771568
EMachad0 <elitonmachadod200@gmail.com>
2025-05-26 16:41:35 -0300

    add rubocop rules

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,7 +1,7 @@
 class Feedback < ApplicationRecord
   FEEDBACK_TYPES = %w[verified reset account_takeover identity_fraud].freeze
 
-  belongs_to :reported_by_user, class_name: 'User', foreign_key: :reported_by_user_id, optional: true, inverse_of: :feedbacks
+  belongs_to :reported_by_user, class_name: "User", foreign_key: :reported_by_user_id, optional: true, inverse_of: :feedbacks
   has_one :feedback_result, dependent: :destroy, inverse_of: :feedback
 
   validates :organization_id, presence: true
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,12 +4,12 @@ RSpec.describe User, type: :model do
   subject { build(:user) }
 
   describe 'associations' do
-  it { is_expected.to have_many(:feedbacks).with_foreign_key(:reported_by_user_id).dependent(:nullify) }
+    it { is_expected.to have_many(:feedbacks).with_foreign_key(:reported_by_user_id).dependent(:nullify) }
   end
 
   describe 'validations' do
-  it { is_expected.to validate_presence_of(:name) }
-  it { is_expected.to validate_presence_of(:email) }
-  it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
   end
 end
