3b4b680b838446e67e9cf77725a7f2100368f3e1
EMachad0 <elitonmachadod200@gmail.com>
2025-05-26 19:31:23 -0300

    fix migrations according to proposed solution

--- db/migrate/20250526184028_create_feedbacks.rb
+++ db/migrate/20250526184028_create_feedbacks.rb
@@ -9,17 +9,9 @@ class CreateFeedbacks < ActiveRecord::Migration[8.0]
       t.string :feedback_type
       t.datetime :feedback_time
 
-      t.index :organization_id
-      t.index :account_id
-      t.index :encoded_installation_id
-      t.index :feedback_type
-      t.index :feedback_time
-      t.index :reported_by_user_id
-
       t.timestamps
     end
 
-    # Foreign-key constraint (optional because users table uses uuid primary key)
     add_foreign_key :feedbacks, :users, column: :reported_by_user_id
   end
 end
--- db/migrate/20250526184054_create_feedback_results.rb
+++ db/migrate/20250526184054_create_feedback_results.rb
@@ -2,6 +2,7 @@ class CreateFeedbackResults < ActiveRecord::Migration[8.0]
   def change
     create_table :feedback_results do |t|
       t.references :feedback, null: false, foreign_key: true
+      t.bigint :organization_id, null: false
       t.integer :affected_devices
       t.integer :estimated_affected_accounts
       t.datetime :processed_time
