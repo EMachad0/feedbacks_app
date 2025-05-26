0f0da941dfb78d6aafa7e66ba643f5f5e97f34da
EMachad0 <elitonmachadod200@gmail.com>
2025-05-26 19:41:48 -0300

    test use describe instead of comments

--- spec/models/feedback_result_spec.rb
+++ spec/models/feedback_result_spec.rb
@@ -3,11 +3,13 @@ require 'rails_helper'
 RSpec.describe FeedbackResult, type: :model do
   subject { build(:feedback_result) }
 
-  # Associations
-  it { is_expected.to belong_to(:feedback) }
+  describe 'associations' do
+    it { is_expected.to belong_to(:feedback) }
+  end
 
-  # Validations
-  it { is_expected.to validate_presence_of(:affected_devices) }
-  it { is_expected.to validate_presence_of(:estimated_affected_accounts) }
-  it { is_expected.to validate_presence_of(:processed_time) }
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:affected_devices) }
+    it { is_expected.to validate_presence_of(:estimated_affected_accounts) }
+    it { is_expected.to validate_presence_of(:processed_time) }
+  end
 end
--- spec/models/feedback_spec.rb
+++ spec/models/feedback_spec.rb
@@ -3,11 +3,13 @@ require 'rails_helper'
 RSpec.describe Feedback, type: :model do
   subject { build(:feedback) }
 
-  # Associations
-  it { is_expected.to belong_to(:reported_by_user).class_name('User').optional }
-  it { is_expected.to have_one(:feedback_result).dependent(:destroy) }
+  describe 'associations' do
+    it { is_expected.to belong_to(:reported_by_user).class_name('User').optional }
+    it { is_expected.to have_one(:feedback_result).dependent(:destroy) }
+  end
 
-  # Validations
-  it { is_expected.to validate_presence_of(:organization_id) }
-  it { is_expected.to validate_presence_of(:feedback_time) }
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:organization_id) }
+    it { is_expected.to validate_presence_of(:feedback_time) }
+  end
 end
